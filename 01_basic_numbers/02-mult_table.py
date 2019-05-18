a8f160848bad3d725f56d1f3c20e4981649050b9
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-18 15:33:04 -0500

    Edit
    Add string formating, clean output

--- 01_basic_numbers/02-mult_table.py
+++ 01_basic_numbers/02-mult_table.py
@@ -3,21 +3,26 @@ Date: Monday July 30, 2018
 '''
 
 '''
-For positive integers a and b, print the multiplication table
-of a up to the b row
+Print the multiplication table of a up to the b row
 '''
 def make_table(a, b = 10):
+    spacing = 5
     if a > 0 and b > 0:
+        print(f'{"a":5}{"b":5}{"a x b"}')
+
         for i in range(1, b + 1):
-            print(a, "X", i, "=", a * i)
+            print(f'{a:<5}{i:<5}{a*i}')
 
     else:
         print(a, "and", b, "both need to be positive integers")
 
+    print()
 
-## Test Method
-if __name__ == "__main__":
-    def test_make_table():
-        make_table(1)
 
-    test_make_table()
+if __name__ == '__main__':
+    a = 5
+    make_table(a)
+
+    a = 7
+    b = 3
+    make_table(a, b)
